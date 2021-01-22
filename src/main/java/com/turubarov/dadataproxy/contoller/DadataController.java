40c0e204c1f9287fef2890ec2b84fe732e8ddd9e
Alex <turubarov88@mail.ru>
2021-01-22 14:00:28 +0700

    fix

--- src/main/java/com/turubarov/dadataproxy/contoller/DadataController.java
+++ src/main/java/com/turubarov/dadataproxy/contoller/DadataController.java
@@ -1,6 +1,5 @@
 package com.turubarov.dadataproxy.contoller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.turubarov.dadataproxy.domain.Address;
 import com.turubarov.dadataproxy.service.AddressService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,6 +26,6 @@ public class DadataController {
     @GetMapping(value = "search")
     public List<Address> search(@RequestParam("type") String type,
                                 @RequestParam("query") String query) throws Exception {
-        return addressService.processRequest(query);
+        return addressService.processSearch(type, query);
     }
 }
